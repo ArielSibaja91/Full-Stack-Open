c1cce4f8ac2f99f61e9f9aa80aaa85fed1774197
Ariel Sibaja <asibaja91@gmail.com>
2025-04-09 07:57:47 -0600

    5.15 Third test implemented

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -40,4 +41,27 @@ test('show content when button is clicked', () => {
 
   expect(div).toHaveTextContent('https://fullstackopen.com')
   expect(div).toHaveTextContent('5')
+})
+
+test('clicking the button twice calls event handler twice', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Ariel Sibaja',
+    url: 'https://fullstackopen.com',
+    likes: 5,
+    user: {
+      id: '643f1a0b2c9e3d4f8c5b6a7e',
+    }
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} addLikes={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('like')
+  await user.click(button)
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
