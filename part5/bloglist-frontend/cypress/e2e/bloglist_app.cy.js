20d74812a302a54a6268703877b9d75f81d8a805
Ariel Sibaja <asibaja91@gmail.com>
2025-04-15 10:49:12 -0600

    5.18 Second e2e test implemented

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,11 +1,33 @@
 describe('Bloglist app', function() {
   beforeEach(function() {
-    cy.visit('http://localhost:5173')
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'test user',
+      username: 'testuser',
+      password: 'testpassword'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.visit("http://localhost:5173/")
   })
 
   it('Login appears in the screen', function() {
     cy.contains('Log into the application')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('testpassword')
+      cy.get('#login-button').click()
+      cy.contains('testuser logged in')
+    })
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+      cy.get('.error').contains('Wrong username or password')
+    })
   })
 })
