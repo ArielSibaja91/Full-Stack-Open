ce550c05cb389f97fd8c3cdb1ffd0d5a276d0970
Ariel Sibaja <asibaja91@gmail.com>
2025-04-30 11:31:58 -0600

    6.24 OnError notification messagge implemented, fixed anecdote form onCreate function

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -8,23 +8,29 @@ const AnecdoteForm = () => {
   
   const newAnecdoteMutation = useMutation({
     mutationFn: createNew,
-    onSuccess: () => {
+    onSuccess: (newAnecdote) => {
       queryClient.invalidateQueries({queryKey: ['anecdotes']})
+      dispatch({ type: 'SHOW_NOTIFICATION', payload: `anecdote ${newAnecdote.content} created` })
+      setTimeout(() => {
+        dispatch({ type: 'HIDE_NOTIFICATION' })
+      }, 5000)
+    },
+    onError: () => {
+      dispatch({ type: 'SHOW_NOTIFICATION', payload: 'too short anecdote, must have length 5 or more' })
+      setTimeout(() => {
+        dispatch({ type: 'HIDE_NOTIFICATION' })
+      }, 5000)
     }
   })
 
   const getId = () => (100000 * Math.random()).toFixed(0)
 
-  const onCreate = async (event) => {
+  const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({content, id: getId(), votes: 0})
     console.log('new anecdote')
-    await dispatch({ type: 'SHOW_NOTIFICATION', payload: `anecdote '${content}' created` })
-    setTimeout(() => {
-      dispatch({ type: 'HIDE_NOTIFICATION' })
-    }, 5000)
 }
 
   return (
