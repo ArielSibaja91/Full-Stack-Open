d3e353fced04b28e1f84c26962f3a8b30cfcc9fb
Ariel Sibaja <asibaja91@gmail.com>
2025-04-30 09:24:40 -0600

    6.21 Create anecdotes implemented

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,22 @@
+import { useQueryClient, useMutation } from '@tanstack/react-query'
+import { createNew } from '../request'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createNew,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['anecdotes']})
+    }
+  })
+
+  const getId = () => (100000 * Math.random()).toFixed(0)
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({content, id: getId(), votes: 0})
     console.log('new anecdote')
 }
 
@@ -12,7 +25,7 @@ const AnecdoteForm = () => {
       <h3>create new</h3>
       <form onSubmit={onCreate}>
         <input name='anecdote' />
-        <button type="submit">create</button>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
--- part6/query-anecdotes/src/request.js
+++ part6/query-anecdotes/src/request.js
@@ -1,3 +1,9 @@
 import axios from 'axios'
 
-export const getAll = () => axios.get('http://localhost:3001/anecdotes').then(response => response.data)
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAll = () => axios.get(`${baseUrl}`).then(response => response.data)
+
+export const createNew = (anecdote) => {
+    axios.post(`${baseUrl}`, anecdote).then(response => response.data)
+}
