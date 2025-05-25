320735959fac4432a551a4a99bab631d99476472
Ariel Sibaja <asibaja91@gmail.com>
2025-05-25 11:44:31 -0600

    *7.21 Changes to deploy the app

--- part7/bloglist-redux/backend/app.js
+++ part7/bloglist-redux/backend/app.js
@@ -9,6 +9,7 @@ const commentsRouter = require('./controllers/comment.routes');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'))
 app.use(middleware.tokenExtractor);
 app.use(middleware.userExtractor);
 app.use('/api/blogs', blogRouter);
